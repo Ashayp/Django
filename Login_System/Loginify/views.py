7aace2dc6680d1e5355216dc9be59932d01fc597
Ashay Pable <apable@stevens.edu>
2024-10-17 01:38:05 -0400

    Basic Login Template Added

--- Login_System/Loginify/views.py
+++ Login_System/Loginify/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+
+def hello_world(request):
+    return HttpResponse("Hello, world!")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+                
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:            
+            login(request, user)
+            return HttpResponse("Login successful!")
+        else:
+            return HttpResponse("Invalid credentials. Please try again.")
+    
+    return render(request, 'Loginify/login.html')
